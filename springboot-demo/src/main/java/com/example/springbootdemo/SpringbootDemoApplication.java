ca21a046445a4da43e8e95a7cf3811ca4fe45edf
jcwangvip <451809860@qq.com>
2019-05-12 15:47:18 +0800

    提交项目搭建骨架

---
+++ springboot-demo/src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author jiancheng
+ */
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
